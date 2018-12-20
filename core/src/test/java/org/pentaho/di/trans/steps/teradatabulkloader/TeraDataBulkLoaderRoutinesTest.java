90e5f36a4c1be5f7c7b899d8ca954d184fca3d9f
Sérgio Samora <sergiosamora@gmail.com>
2018-12-20 16:57:20 +0000

    [SP-4832] Backport of PDI-17750 - Teradata TPT Bulk Loader Step does not take into account the Work Table field. (8.2 Suite)

--- core/src/test/java/org/pentaho/di/trans/steps/teradatabulkloader/TeraDataBulkLoaderRoutinesTest.java
+++ core/src/test/java/org/pentaho/di/trans/steps/teradatabulkloader/TeraDataBulkLoaderRoutinesTest.java
@@ -23,6 +23,9 @@ package org.pentaho.di.trans.steps.teradatabulkloader;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.util.reflection.Whitebox;
+import org.pentaho.di.core.database.DatabaseMeta;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.junit.Assert.assertEquals;
@@ -32,11 +35,13 @@ public class TeraDataBulkLoaderRoutinesTest {
   private TeraDataBulkLoaderRoutines teraDataBulkLoaderRoutines;
   private TeraDataBulkLoader teraDataBulkLoaderMock;
   private TeraDataBulkLoaderMeta teraDataBulkLoaderMetaMock;
+  private DatabaseMeta dataBaseMetaDataMock;
 
   @Before
   public void setup() {
     teraDataBulkLoaderMock = mock( TeraDataBulkLoader.class );
     teraDataBulkLoaderMetaMock = mock( TeraDataBulkLoaderMeta.class );
+    dataBaseMetaDataMock = mock( DatabaseMeta.class );
     teraDataBulkLoaderRoutines = new TeraDataBulkLoaderRoutines( teraDataBulkLoaderMock, teraDataBulkLoaderMetaMock );
   }
 
@@ -59,5 +64,125 @@ public class TeraDataBulkLoaderRoutinesTest {
     when( teraDataBulkLoaderMetaMock.getSchemaName() ).thenReturn( null );
     when( teraDataBulkLoaderMetaMock.getDbName() ).thenReturn( "dbName" );
     assertEquals("dbName", teraDataBulkLoaderRoutines.getTargetSchema() );
+  }
+
+  @Test
+  public void getDataConnectorTest() {
+    when( teraDataBulkLoaderMetaMock.getSchemaName() ).thenReturn( "schemaName" );
+    when( teraDataBulkLoaderMetaMock.getAccessLogFile() ).thenReturn( "accessLog" );
+    when( teraDataBulkLoaderMetaMock.getFifoFileName() ).thenReturn( "fifoFileName" );
+    Whitebox.setInternalState( teraDataBulkLoaderRoutines, "parent", null );
+    String expectedDataConnector = "DEFINE OPERATOR ACCESS_MODULE_READER\n"
+      + "TYPE DATACONNECTOR PRODUCER\n"
+      + "SCHEMA schemaName\n"
+      + "ATTRIBUTES\n"
+      + "  (\n"
+      + "     VARCHAR PrivateLogName = 'accessLog',\n"
+      + "     VARCHAR AccessModuleName = 'np_axsmod.so',\n"
+      + "     VARCHAR FileName = 'fifoFileName',\n"
+      + "     VARCHAR Format = 'Unformatted',\n"
+      + "     VARCHAR OpenMode = 'Read'\n"
+      + "  );\n";
+    assertEquals( expectedDataConnector, teraDataBulkLoaderRoutines.getDataConnector().toString() );
+  }
+
+  @Test
+  public void getDdlOptionsPreviewTest() {
+    when( teraDataBulkLoaderMetaMock.getDatabaseMeta() ).thenReturn( dataBaseMetaDataMock );
+    when( dataBaseMetaDataMock.getHostname() ).thenReturn( "hostname" );
+    when( dataBaseMetaDataMock.getUsername() ).thenReturn( "username" );
+    String password = "myPassword";
+    String expectedDdlOptionsPreview = "DEFINE OPERATOR DDL_OPERATOR\n"
+      + "TYPE DDL\n"
+      + "ATTRIBUTES\n"
+      + "  (\n"
+      + "     VARCHAR TdpId              = 'hostname',\n"
+      + "     VARCHAR UserName           = 'username',\n"
+      + "     VARCHAR UserPassword       = 'myPassword',\n"
+      + "     VARCHAR ErrorList          = '3807'\n"
+      + "  );\n";
+    assertEquals( expectedDdlOptionsPreview, teraDataBulkLoaderRoutines.getDdlOptions( true, password ).toString() );
+  }
+
+  @Test
+  public void getDdlOptionsTest() {
+    when( teraDataBulkLoaderMetaMock.getDatabaseMeta() ).thenReturn( dataBaseMetaDataMock );
+    when( dataBaseMetaDataMock.getHostname() ).thenReturn( "hostname" );
+    when( dataBaseMetaDataMock.getUsername() ).thenReturn( "username" );
+    when( dataBaseMetaDataMock.getPassword() ).thenReturn( "passwordFromMeta" );
+    String password = "myPassword";
+    String expectedDdlOptionsPreview = "DEFINE OPERATOR DDL_OPERATOR\n"
+      + "TYPE DDL\n"
+      + "ATTRIBUTES\n"
+      + "  (\n"
+      + "     VARCHAR TdpId              = 'hostname',\n"
+      + "     VARCHAR UserName           = 'username',\n"
+      + "     VARCHAR UserPassword       = 'passwordFromMeta',\n"
+      + "     VARCHAR ErrorList          = '3807'\n"
+      + "  );\n";
+    assertEquals( expectedDdlOptionsPreview, teraDataBulkLoaderRoutines.getDdlOptions( false, password ).toString() );
+  }
+
+  @Test
+  public void getUpdateOptionsPreviewTest() {
+    when( teraDataBulkLoaderMetaMock.getDatabaseMeta() ).thenReturn( dataBaseMetaDataMock );
+    when( dataBaseMetaDataMock.getHostname() ).thenReturn( "hostname" );
+    when( dataBaseMetaDataMock.getUsername() ).thenReturn( "username" );
+    when( teraDataBulkLoaderMetaMock.getUpdateLogFile() ).thenReturn( "logFile" );
+    when( teraDataBulkLoaderMetaMock.getLogTable() ).thenReturn( "logTable" );
+    when( teraDataBulkLoaderMetaMock.getTableName() ).thenReturn( "tableName" );
+    when( teraDataBulkLoaderMetaMock.getWorkTable() ).thenReturn( "workTable" );
+    when( teraDataBulkLoaderMetaMock.getErrorTable() ).thenReturn( "errorTable" );
+    when( teraDataBulkLoaderMetaMock.getErrorTable2() ).thenReturn( "errorTable2" );
+    when( teraDataBulkLoaderMetaMock.getDbName() ).thenReturn( "dbName" );
+    String password = "myPassword";
+    String expectedUpdataOptionsPreview = "DEFINE OPERATOR UPDATE_OPERATOR\n"
+      + "TYPE UPDATE\n"
+      + "SCHEMA *\n"
+      + "ATTRIBUTES\n"
+      + "  (\n"
+      + "     VARCHAR TdpId              = 'hostname',\n"
+      + "     VARCHAR PrivateLogName     = 'logFile',\n"
+      + "     VARCHAR UserName           = 'username',\n"
+      + "     VARCHAR UserPassword       = 'myPassword',\n"
+      + "     VARCHAR LogTable           = 'logTable',\n"
+      + "     VARCHAR TargetTable        = 'dbName.tableName',\n"
+      + "     VARCHAR WorkTable          = 'workTable',\n"
+      + "     VARCHAR ErrorTable1        = 'errorTable',\n"
+      + "     VARCHAR ErrorTable2        = 'errorTable2'\n"
+      + "  );\n";
+    assertEquals( expectedUpdataOptionsPreview, teraDataBulkLoaderRoutines.getUpdateOptions( true, password ).toString() );
+  }
+
+  @Test
+  public void getUpdateOptionsTest() {
+    when( teraDataBulkLoaderMetaMock.getDatabaseMeta() ).thenReturn( dataBaseMetaDataMock );
+    when( dataBaseMetaDataMock.getHostname() ).thenReturn( "hostname" );
+    when( dataBaseMetaDataMock.getUsername() ).thenReturn( "username" );
+    when( dataBaseMetaDataMock.getPassword() ).thenReturn( "passwordFromMeta");
+    when( teraDataBulkLoaderMetaMock.getUpdateLogFile() ).thenReturn( "logFile" );
+    when( teraDataBulkLoaderMetaMock.getLogTable() ).thenReturn( "logTable" );
+    when( teraDataBulkLoaderMetaMock.getTableName() ).thenReturn( "tableName" );
+    when( teraDataBulkLoaderMetaMock.getWorkTable() ).thenReturn( "workTable" );
+    when( teraDataBulkLoaderMetaMock.getErrorTable() ).thenReturn( "errorTable" );
+    when( teraDataBulkLoaderMetaMock.getErrorTable2() ).thenReturn( "errorTable2" );
+    when( teraDataBulkLoaderMetaMock.getDbName() ).thenReturn( "dbName" );
+    String password = "myPassword";
+    String expectedUpdataOptionsPreview = "DEFINE OPERATOR UPDATE_OPERATOR\n"
+      + "TYPE UPDATE\n"
+      + "SCHEMA *\n"
+      + "ATTRIBUTES\n"
+      + "  (\n"
+      + "     VARCHAR TdpId              = 'hostname',\n"
+      + "     VARCHAR PrivateLogName     = 'logFile',\n"
+      + "     VARCHAR UserName           = 'username',\n"
+      + "     VARCHAR UserPassword       = 'passwordFromMeta',\n"
+      + "     VARCHAR LogTable           = 'logTable',\n"
+      + "     VARCHAR TargetTable        = 'dbName.tableName',\n"
+      + "     VARCHAR WorkTable          = 'workTable',\n"
+      + "     VARCHAR ErrorTable1        = 'errorTable',\n"
+      + "     VARCHAR ErrorTable2        = 'errorTable2'\n"
+      + "  );\n";
+    assertEquals( expectedUpdataOptionsPreview, teraDataBulkLoaderRoutines.getUpdateOptions( false, password ).toString() );
   }
 }
